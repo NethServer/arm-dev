78dde209d38a704d5f0aa4ffba460d98732a7f7c
Mark Verlinde <mark@havak.nl>
2018-10-20 14:27:11 +0200

    Add scripts for download from google drive

---
+++ curl_Generic_google_drive.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# thanks to Brett McLain
+# http://blog.mclain.ca/download-large-files-from-google-drive-via-curl/
+
+filename="Nethserver-7.5.1804-Devel-Generic-img.raw.xz"
+file_id="1A5EUMJ6ZI2rRKidyX3Nc2NFWGCtiFCVp"
+
+query=`curl -c ./cookie.txt -s -L "https://drive.google.com/uc?export=download&id=${file_id}" \
+| perl -nE'say/uc-download-link.*? href="(.*?)\">/' \
+| sed -e 's/amp;//g' | sed -n 2p`
+url="https://drive.google.com$query"
+curl -b ./cookie.txt -L -o ${filename} $url
+
+rm -f cookie.txt
